ce4b0895321198c03e4c1829a4ad686f76b89c34
AvnishR <71318917+Avnish-Raut@users.noreply.github.com>
2022-05-02 18:00:02 +0530

    Add files via upload

---
+++ Practical/char_pro.java
@@ -0,0 +1,14 @@
+package hello_world;
+
+public class char_pro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char marks,grade;
+		marks = '8';
+		grade = 'B';
+		System.out.println("Marks : "+ marks);
+		System.out.println("Grade : "+ grade);
+	}
+
+}
