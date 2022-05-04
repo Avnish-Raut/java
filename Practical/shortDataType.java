9de5218a9b117873303360f5e0499bdbec7f4aa3
AvnishR <71318917+Avnish-Raut@users.noreply.github.com>
2022-05-04 18:04:59 +0530

    Add files via upload

---
+++ Practical/shortDataType.java
@@ -0,0 +1,11 @@
+package hello_world;
+
+public class shortDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short myShort = 32767;
+		System.out.println("Short : "+myShort);
+	}
+
+}
