bfa8f31de3e23c16ed55f984b13e0d47558b682f
AvnishR <71318917+Avnish-Raut@users.noreply.github.com>
2022-05-04 18:17:41 +0530

    Add files via upload

---
+++ Assignement/Assignement_1.java
@@ -0,0 +1,21 @@
+package hello_world;
+
+public class Assignement_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1,num2,add,sub,mul,div;
+		num1 = 200;
+		num2 = 50;
+		add = num1 + num2;
+		sub = num1 - num2;
+		mul = num1 * num2;
+		div = num1 / num2;
+		System.out.println("Num 1 : "+num1 + "\nNum2 : " + num2);
+		System.out.println("Addition : "+add);
+		System.out.println("Subtraction : "+sub);
+		System.out.println("Multiplication : "  +mul);
+		System.out.println("Division : "+div);
+	}
+
+}
